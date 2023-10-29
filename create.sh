90c611340797c71cc6357635b997a1324a6e4867
alyn03 <147057014+alyn03@users.noreply.github.com>
2023-10-29 15:54:11 +0000

    Update create.sh

--- create.sh
+++ create.sh
@@ -3,21 +3,22 @@
 #Takes in 1 argument - id
 #Creating a user
 
-path="Users"
+path="Users" #variable that holds the pathway
 
-if [ "$#" -eq 1 ]; then
-	arg="$1"
+if [ "$#" -eq 1 ]; then #one and only 1 arg
+	arg="$1" #set the argument to variable arg
 	if [ -d "/home/ranya/Desktop/Users/$arg" ]; then #check if user folder already exists
-		echo "User already exists"
-	else
-		mkdir -p "$path/$arg"
-		touch "$path/$arg/wall.txt" 
-		touch "$path/$arg/friends.txt"
-		echo "Start_Of_File" >> "$path/$arg/wall.txt"
-		echo "$arg" >> "$path/$arg/friends.txt"
+		echo "User already exists" #if it's found that means it was previously created
+	else 
+ 		#user doesn't exist
+		mkdir -p "$path/$arg" #creates a directory with the name of the argument
+		touch "$path/$arg/wall.txt" #creates a file called wall.txt
+		touch "$path/$arg/friends.txt" #creates a file called friends.txt
+		echo "Start_Of_File" >> "$path/$arg/wall.txt" #adds "Start_Of_File" to the wall.txt
+		echo "$arg" >> "$path/$arg/friends.txt" #adds yourself to your own friends.txt, this allows you to write on your own wall
 		echo "User created successfully"
 	fi
 else
-	echo "Usage: $0 <arg>"
-	exit 1
+	echo "Usage: $0 <arg>" #wrong amount of args given
+	exit 1 #failed
 fi
