25bab9ea339ec78f978e71edfad4053bee75ca4c
alyn03 <147057014+alyn03@users.noreply.github.com>
2023-10-29 16:01:03 +0000

    Update post_message.sh

--- post_message.sh
+++ post_message.sh
@@ -2,19 +2,21 @@
 
 #takes in 3 arguements, sender, reciever and message
 
-if [ "$#" -eq 3 ]; then
+if [ "$#" -eq 3 ]; then #3 and only 3 args
 	sender="$1"
 	receiver="$2"
 	message="$3"
-	if [ -d "/home/ranya/Desktop/Users/$sender" ]; then
-		if [ -d "/home/ranya/Desktop/Users/$receiver" ]; then
-			if  grep -q "$sender" "/home/ranya/Desktop/Users/$receiver/friends.txt" ; then
-				echo "$sender: $message" >> "/home/ranya/Desktop/Users/$receiver/wall.txt"
-				echo "$message has been sent to $receiver"
+	if [ -d "/home/ranya/Desktop/Users/$sender" ]; then #checking existence
+		if [ -d "/home/ranya/Desktop/Users/$receiver" ]; then #checking existence
+			if  grep -q "$sender" "/home/ranya/Desktop/Users/$receiver/friends.txt" ; then #they have to be on the friend's list
+   				#if they are friends:
+				echo "$sender: $message" >> "/home/ranya/Desktop/Users/$receiver/wall.txt" #sender's name : "the message" onto the wall.txt
+    				# >> operator adds things to txt files
+				echo "$message has been sent to $receiver" #pirnts to terminal that it was successful
 				exit 0	
 			
 			else
-				echo "Sender and receiver are not friends"
+				echo "Sender and receiver are not friends" 
 				exit 1
 			fi
 		else
