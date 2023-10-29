441018807bcc11028e30fdec9470917a87f67db2
alyn03 <147057014+alyn03@users.noreply.github.com>
2023-10-29 15:59:04 +0000

    Update add_friend.sh

--- add_friend.sh
+++ add_friend.sh
@@ -2,18 +2,19 @@
 
 # Takes in 2 arguments - id for user and id for friend
 
-if [ "$#" -eq 2 ]; then
-	id="$1"
-	friend="$2"
-	if [ -d "/home/ranya/Desktop/Users/$id" ]; then
-		if [ -d "/home/ranya/Desktop/Users/$friend" ]; then
-			if ! grep -q "$friend" "/home/ranya/Desktop/Users/$id/friends.txt"; then
-				echo "$friend" >> "/home/ranya/Desktop/Users/$id/friends.txt"
-				echo "$id" >> "/home/ranya/Desktop/Users/$friend/friends.txt"
+if [ "$#" -eq 2 ]; then #2 and only 2 args
+	id="$1" #1st arg
+	friend="$2" #2nd arg
+	if [ -d "/home/ranya/Desktop/Users/$id" ]; then #check if 1st user exists
+		if [ -d "/home/ranya/Desktop/Users/$friend" ]; then #check if 2nd user exists
+			if ! grep -q "$friend" "/home/ranya/Desktop/Users/$id/friends.txt"; then #checking if friend's value is already somewhere on the file
+				echo "$friend" >> "/home/ranya/Desktop/Users/$id/friends.txt" #echo (print) the friend's value to the 1st user's friend list
+				echo "$id" >> "/home/ranya/Desktop/Users/$friend/friends.txt" #printing the id's value to the 2nd user's list
+    				#friends on facebook are synchronised, not the same as a follower
 				echo "Friend added successfully"
 				exit 0
 			else
-				echo "Friend already on friends list"
+				echo "Friend already on friends list" #if the name was found on the file
 				exit 1
 			fi
 		else
@@ -25,6 +26,6 @@ if [ "$#" -eq 2 ]; then
 		exit 1
 	fi
 else
-	echo "Incorrect number of arguments given, you had $# and you need 2"
+	echo "Incorrect number of arguments given, you had $# and you need 2" #wrong amount of args
 fi
 
