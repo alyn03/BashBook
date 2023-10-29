015276f9a7a73ee1da898eafbd70582f90685172
ranyaprax <124516985+ranyaprax@users.noreply.github.com>
2023-10-29 15:59:23 +0000

    Update display_wall.sh

--- display_wall.sh
+++ display_wall.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-#takes in id of user that the wall belongs to 
+#Takes in id of user that the wall belongs to 
 
 
-if [ "$#" -eq 1 ]; then
-	id="$1"
-	if [ -d "/home/ranya/Desktop/Users/$id" ]; then
-		cat /home/ranya/Desktop/Users/$id/wall.txt
-		echo "end_of_file"
+if [ "$#" -eq 1 ]; then #check if there are the correct amount of arguements
+	id="$1" #arguement given is the user who will have their wall displayed
+	if [ -d "/home/ranya/Desktop/Users/$id" ]; then #check if the user exists
+		cat /home/ranya/Desktop/Users/$id/wall.txt #print the wall.txt to the terminal
+		echo "end_of_file" #indicate the end of the file
 		exit 0
 	else
 		echo "user does not exist"
