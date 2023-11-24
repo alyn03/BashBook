ed704665e44ab66d454ac78bcf1f3df0a8c74693
alyn03 <147057014+alyn03@users.noreply.github.com>
2023-11-24 13:19:06 +0000

    added comments

--- new/server.sh
+++ new/server.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#initialising our named pipes
 client_pipe="/home/ranya/Desktop/Users/Pipes/$id/client"
 server_pipe="/home/ranya/Desktop/Users/Pipes/server"
 
@@ -8,30 +9,33 @@ if [ ! -e "$server_pipe" ]; then
 	mkfifo "$server_pipe"
 fi
 
+#infinite loop
 while true; do
-	echo "Server side"
+	echo "Server side" #checking where code exits upon error
 
-	temp=$(cat "$server_pipe")
+	temp=$(cat "$server_pipe") #this holds the temporary value that is taken from the server pipe - sent in from a client
 	
 
 	IFS=' ' #setting space as delimiter   (internal field separator)
 	read -ra messagelist <<<"$temp" #reading str as an array as tokens separated by IFS  
-  
+
+  	#seperating the elements into separate variables
 	cmd="${messagelist[0]}"
 	id="${messagelist[1]}"
 	args="${messagelist[2]}"
 	echo "command: $cmd, id: $id, args: $args"
 
-	
+	#command ("cmd") will be 1 of 4 things create/post/display/add
 	case "$cmd" in 
 		"create")
 		#acquire
 		#echo $(./acquire.sh c1)
 		echo "create"
+  		#calling the bash script create.sh with the needed parameters
 		./create.sh $id
 		#echo "$res" > $client_pipe
 		#./create.sh "$id" > $client_pipe
-		test=$(<$client_pipe)
+		test=$(<$client_pipe) #assigning the values in the client pipe to a temporary variable "test"
 		echo "test: $test"
 		exit
 		#release
