c8c5f133a5de0f65b34ebbf58bfdfe96ff8d31b5
alyn03 <147057014+alyn03@users.noreply.github.com>
2023-11-26 11:02:16 +0000

    comments done

--- BrandNew/post_message.sh
+++ BrandNew/post_message.sh
@@ -2,15 +2,15 @@
 
 #takes in 3 arguements, sender, reciever and message
 
-if [ "$#" -eq 3 ]; then
+if [ "$#" -eq 3 ]; then #3 and only 3 args
 	sender="$1"
 	receiver="$2"
 	message="$3"
-	if [ -d "/home/ranya/Desktop/Users/$sender" ]; then
-		if [ -d "/home/ranya/Desktop/Users/$receiver" ]; then
-			if  grep -q "$sender" "/home/ranya/Desktop/Users/$receiver/friends.txt" ; then
-				echo "$sender: $message" >> "/home/ranya/Desktop/Users/$receiver/wall.txt"
-				#echo "$message has been sent to $receiver"
+	if [ -d "/home/ranya/Desktop/Users/$sender" ]; then #checking existence
+		if [ -d "/home/ranya/Desktop/Users/$receiver" ]; then #checking existence
+			if  grep -q "$sender" "/home/ranya/Desktop/Users/$receiver/friends.txt" ; then #they have to be on the friend's list
+				echo "$sender: $message" >> "/home/ranya/Desktop/Users/$receiver/wall.txt" #sender's name : "the message" onto the wall.txt
+				# >> operator adds things to txt files
 				echo "Message sent"
 				exit 0	
 			
