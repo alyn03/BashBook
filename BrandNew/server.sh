2bb594d5e69528d78b6cc469331dbb6006cd2882
alyn03 <147057014+alyn03@users.noreply.github.com>
2023-11-26 10:53:00 +0000

    comments done

--- BrandNew/server.sh
+++ BrandNew/server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-
+#initialise named pipe
 server_pipe="/home/ranya/Desktop/Users/Pipes/server"
 
 #check if server pipe exists, otherwise create using mkfifo
@@ -8,27 +8,33 @@ if [ ! -e "$server_pipe" ]; then
 	mkfifo "$server_pipe"
 fi
 
+#infinite loop
 while true; do
-	echo "Server side"
+	echo "Server side" #checking where code exits upon error
 
-	temp=$(cat "$server_pipe")
+	temp=$(cat "$server_pipe") #this holds the temporary value that is taken from the server pipe - sent in from a client
 	
 
 	IFS=' ' #setting space as delimiter   (internal field separator)
 	read -ra messagelist <<<"$temp" #reading str as an array as tokens separated by IFS  
-  
+
+ 	#seperating the elements into separate variables
 	cmd="${messagelist[0]}"
 	id="${messagelist[1]}"
 	args1="${messagelist[2]}"
 	args2="${messagelist[3]}"
 	echo "command: $cmd, id: $id, args: $args1, $args2"
-	
+
+ 	#now that we have the id of the current client being run, we can access their pipe
 	client_pipe="/home/ranya/Desktop/Users/Pipes/$id/client"
-	
+
+ 	#command ("cmd") will be 1 of 4 things create/post/display/add
+  	#when you attempt to do an operation, you gain the lock for that operation
 	case "$cmd" in 
 		"create")
 		#acquire
 		echo $(./acquire.sh c1)
+  		#call the bash script needed and give it the correct arg, send result down client pipe
 		./create.sh "$id" > $client_pipe
 		#release
 		echo $(./release.sh)
